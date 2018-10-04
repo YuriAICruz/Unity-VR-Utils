33100d7a63ceb2d53b4b11e3178b30f512ed9777
Yuri Cruz <yuri.cruz@vldomain.vl.com>
2018-10-04 20:06:09 -0300

    hand mapping

---
+++ Graphene/VRUtils/HandPosition.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+namespace Graphene.VRUtils
+{
+    public class HandPosition : MonoBehaviour
+    {
+        public XRNode Point;
+
+        private bool _tracking;
+        private List<XRNodeState> _nodes;
+        private Vector3 _position;
+        private Quaternion _rotation;
+
+        private void Start()
+        {
+//            transform.SetParent(Camera.main.transform);
+
+            InputTracking.nodeAdded += NodeAdd;
+            InputTracking.nodeRemoved += NodeRemove;
+            _nodes = new List<XRNodeState>();
+        }
+
+        private void NodeRemove(XRNodeState node)
+        {
+            if (node.nodeType == Point)
+                _tracking = false;
+        }
+
+        private void NodeAdd(XRNodeState node)
+        {
+            if (node.nodeType == Point)
+                _tracking = true;
+        }
+
+        private void Update()
+        {
+            if (!_tracking || Point == XRNode.GameController || Point == XRNode.TrackingReference || Point == XRNode.HardwareTracker) return;
+
+            InputTracking.GetNodeStates(_nodes);
+
+            foreach (var node in _nodes)
+            {
+                if (node.nodeType != Point) continue;
+
+                if (node.TryGetPosition(out _position))
+                {
+                    transform.localPosition = _position;
+                }
+                if (node.TryGetRotation(out _rotation))
+                {
+                    transform.localRotation = _rotation;
+                }
+            }
+        }
+    }
+}
