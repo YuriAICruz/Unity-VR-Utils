2e385eac91b6645546b25af5710e8be89cc7b9d0
Yuri Cruz <yuriaicruz@gmail.com>
2018-11-22 00:06:11 -0200

    add video url navigation

---
+++ Graphene/VRUtils/StaticNavigation/VideoUrlNavigation.cs
@@ -0,0 +1,60 @@
+﻿using System.Linq;
+using UnityEngine;
+using UnityEngine.Video;
+
+namespace Graphene.VRUtils.StaticNavigation
+{
+    [RequireComponent(typeof(VideoPlayer))]
+    public class VideoUrlNavigation : BaseNavigation
+    {
+        public string[] VideoUrls;
+        private VideoPlayer _player;
+
+        private void Awake()
+        {
+            Textures = new Texture[VideoUrls.Length].ToList();
+
+            _player = GetComponent<VideoPlayer>();
+        }
+
+        protected override void SetMainTexture()
+        {
+            if (_player == null)
+                _player = GetComponent<VideoPlayer>();
+            if (string.IsNullOrEmpty(VideoUrls[_currentTexture]) || _player == null)
+            {
+                if (_player != null)
+                    _player.Stop();
+                
+                return;
+            }
+
+            _player.Play();
+        }
+
+        protected override void SetSecodaryTexture()
+        {
+            if (_player == null)
+                _player = GetComponent<VideoPlayer>();
+            if (string.IsNullOrEmpty(VideoUrls[_currentTexture]) || _player == null)
+            {
+                if (_player != null)
+                    _player.Stop();
+                
+                return;
+            }
+
+            _player.Stop();
+            _player.url = VideoUrls[_currentTexture];
+        }
+
+        protected override void SetUpdateBlend(float t)
+        {
+        }
+
+        protected override void GetMaterial()
+        {
+            base.GetMaterial();
+        }
+    }
+}
