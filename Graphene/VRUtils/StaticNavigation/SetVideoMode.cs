ff3ea35cb98c0cff247af63fcf1112ffbd2de6bb
Willian <wpgermano@gmail.com>
2019-01-04 16:50:21 -0200

    Updated for the new version of the app, that includes the ionic Dynapac News app redone in Unity.

--- Graphene/VRUtils/StaticNavigation/SetVideoMode.cs
+++ Graphene/VRUtils/StaticNavigation/SetVideoMode.cs
@@ -17,6 +17,8 @@ namespace Graphene.VRUtils.StaticNavigation
 
 		private void Awake()
 		{
+            Screen.orientation = ScreenOrientation.LandscapeLeft;
+
 			XRSettings.enabled = false;
 	
 			instructionsPanel = GameObject.Find("MainMenuCanvas/InstructionsPanel");
@@ -43,22 +45,35 @@ namespace Graphene.VRUtils.StaticNavigation
 
 		private IEnumerator WaitForClickToContinue()
 		{
-			string help = "Instruções\n\n";
-			help += STEREO_MODE ? "Coloque os óculos de VR e vire a cabeça para ver as cenas em 360°." : "Deslize o dedo na tela para girar a cena.";
-			help += " Para mudar de sala centralize o cursor por alguns segundos no círculo com o nome do ambiente escolhido.";
-			help += "\n\nNecessário ter conexão de boa velocidade (Wi-Fi).";
+			string help_pt = "Instruções\n\n";
+			help_pt += STEREO_MODE ? "Coloque os óculos de VR e vire a cabeça para ver as cenas em 360°." : "Deslize o dedo na tela para girar a cena.";
+			help_pt += " Para mudar de sala centralize o cursor por alguns segundos no círculo com o nome do ambiente escolhido.";
+			help_pt += "\n\nNecessário ter conexão de boa velocidade (Wi-Fi).";
+
+			string help_es = "Instrucciones\n\n";
+			help_es += STEREO_MODE ? "Coloque las gafas de VR y gire la cabeza para ver las escenas en 360°." : "Deslice el dedo en la pantalla para girar la escena.";
+			help_es += " Para cambiar de sala centraliza el cursor por unos segundos en el círculo con el nombre del ambiente elegido.";
+			help_es += "\n\nEs necesario tener conexión de buena velocidad (Wi-Fi).";
+
 			string lastLine = "\n\n...";
+
+			string touchTheScreen_pt = "\n\nToque na tela para iniciar...";
+			string touchTheScreen_es = "\n\nToque la pantalla para iniciar...";
+
+			// XXX Localization:
+			string help = help_pt;
+			string touchTheScreen = touchTheScreen_pt;
 
 			Text txt = instructionsPanel.GetComponentInChildren<Text>(true);
 
-			txt.text = help + lastLine;
+			txt.text = $"{help}{lastLine}";
 
 			holder.SetActive(false);
 			instructionsPanel.SetActive(true);
 
 			yield return new WaitForSeconds(3); // wait at least 5 seconds
 
-			txt.text = help + "\n\nToque na tela para iniciar...";
+			txt.text = $"{help}{touchTheScreen}";
 			continueBt.interactable = true;
 		}
 	}
