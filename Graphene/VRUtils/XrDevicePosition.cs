64263c820e77347830e3590c3705ee95ac88dde4
Yuri Cruz <yuri.cruz@vldomain.vl.com>
2019-03-25 14:35:42 -0300

    head network

--- Graphene/VRUtils/XrDevicePosition.cs
+++ Graphene/VRUtils/XrDevicePosition.cs
@@ -9,7 +9,7 @@ namespace Graphene.VRUtils
     {
         public XRNode Point;
 
-        private bool _tracking;
+        private bool _tracking = true;
         private List<XRNodeState> _nodes;
         private Vector3 _position;
         public Vector3 Offset;
@@ -48,7 +48,7 @@ namespace Graphene.VRUtils
 
                 if (node.TryGetPosition(out _position))
                 {
-                    transform.localPosition = _position+Offset;
+                    transform.localPosition = _position + Offset;
                 }
                 if (node.TryGetRotation(out _rotation))
                 {
