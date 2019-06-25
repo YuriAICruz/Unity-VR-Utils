79fc10b74a02a0daa4123ab79df861a6dd9d3b0f
Yuri Cruz <yuri.cruz@vldomain.vl.com>
2019-06-25 15:20:33 -0300

    vr teleporter

---
+++ Graphene/VRUtils/VRTeleporter/Sample/SampleVRTeleporterController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Graphene.VRUtils
+{
+    public class SampleVRTeleporterController : MonoBehaviour
+    {
+
+        public VRTeleporter teleporter;
+
+        void Update()
+        {
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                teleporter.ToggleDisplay(true);
+            }
+
+            if (Input.GetMouseButtonUp(0))
+            {
+                teleporter.Teleport();
+                teleporter.ToggleDisplay(false);
+            }
+        }
+    }
+}
